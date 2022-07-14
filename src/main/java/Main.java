e7cfede60be1cc8d1ed1ff1283c1df0eb00463b3
ArchangelosZP <archangeloszp@gmail.com>
2022-07-14 01:01:44 -0400

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        RomanToInteger rtl = new RomanToInteger();
-        System.out.print("Enter Roman numeral text >");
-        Scanner scanner = new Scanner(System.in);
-        String roman = scanner.nextLine();
-        rtl.convertRomanToInt(roman);
+
     }
 }
